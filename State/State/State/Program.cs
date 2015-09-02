7ec76a63fa6bb3132e33020604323e1e2876904d
Oscar Palma <oskr.406@gmail.com>
2015-09-02 16:34:58 -0700

    PDD State ya Funcional

---
+++ State/State/State/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+namespace State
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Application.Init ();
+			//MainWindow win = new MainWindow ();
+			//win.Show ();
+			//Application.Run ();
+
+			Vehiculo v = new Vehiculo(20);
+
+			v.Acelerar();
+			v.Contacto();
+			v.Acelerar();
+			v.Acelerar();
+			v.Acelerar();
+			v.Frenar();
+			v.Frenar();
+			v.Frenar();
+			v.Frenar();
+			Console.Read ();
+		}
+	}
+}
