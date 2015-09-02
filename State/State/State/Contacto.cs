217b1679b8ff93fc785e0de023a693f6b2005615
Miriam Ildefonso <mildefonso1@gmail.com>
2015-09-03 00:59:32 +0200

    SIngleton FUncionando

---
+++ State/State/State/Contacto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace State
+{
+	public class Contacto
+	{
+		
+		public Contacto ()
+		{
+		}
+		switch (estado)
+		{
+		case APAGADO:
+			{
+				// El vehiculo arranca
+				estado = PARADO;
+				velocidadActual = 0;
+				Console.WriteLine("El vehiculo se encuentra ahora PARADO");
+				break;
+			}
+		case PARADO:
+			{
+				// El vehiculo se apaga
+				estado = APAGADO;
+				Console.WriteLine("El vehiculo se encuentra ahora APAGADO");
+				break;
+			}
+		case EN_MARCHA:
+			{
+				// No se puede detener el vehiculo en marcha!
+				Console.WriteLine("ERROR: No se puede cortar el contacto en marcha!");
+				break;
+			}
+		default:
+			break;
+		}
+	}
+}
+
